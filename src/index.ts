586a10874a7b069e143dbaeee365736009b43fc8
Ugo Arzur <ugo.arzur@fabernovel.ey.com>
2025-03-07 17:22:34 +0100

    visited memory

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,25 @@ import axios from 'axios'
 import { urls, allowedTypes } from '../config.json'
 
 const IMAGES_DIR = path.join(__dirname, '..', 'images')
+const VISITED_URLS = path.join(__dirname, 'visited.json')
+
+function loadVisitedUrls(): Set<string> {
+  if (fs.existsSync(VISITED_URLS)) {
+    return new Set(JSON.parse(fs.readFileSync(VISITED_URLS, 'utf-8')))
+  }
+  return new Set()
+}
+
+function saveVisitedUrls(visited: Set<string>) {
+  fs.writeFileSync(VISITED_URLS, JSON.stringify(Array.from(visited), null, 2))
+}
 
 async function downloadImage(url: string, folder: string, filename: string) {
   try {
     const response = await axios({ url, responseType: 'arraybuffer' })
     fs.writeFileSync(path.join(folder, filename), response.data)
   } catch (error) {
-    console.error(`Erreur lors du téléchargement de ${url}:`, error)
+    console.error(`Error while downloading from ${url}:`, error)
   }
 }
 
@@ -42,7 +54,6 @@ async function scrapeImages(url: string) {
     const filename = path.basename(new URL(imageUrl).pathname)
     await downloadImage(imageUrl, pageFolder, filename)
   }
-
   await browser.close()
 }
 
@@ -50,10 +61,18 @@ async function scrapeImages(url: string) {
   if (!fs.existsSync(IMAGES_DIR)) {
     fs.mkdirSync(IMAGES_DIR)
   }
+  const visitedUrls: Set<string> = loadVisitedUrls()
 
   for (const url of urls) {
+    if (visitedUrls.has(url)) {
+      console.log(`Skipping ${url}, already visited 👍.`)
+      continue
+    }
+
     console.log(`Scraping ${url}...`)
     await scrapeImages(url)
+    visitedUrls.add(url)
+    saveVisitedUrls(visitedUrls)
   }
-  console.log('Scraping terminé.')
+  console.log('Scraping finished ✅')
 })()
